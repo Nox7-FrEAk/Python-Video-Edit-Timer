0e7f9b4d506941908af6175100310d66d7198da5
Maximilian B <M.blinzler@outlook.de>
2019-11-05 22:10:55 +0100

    Config test

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ s = 'Key.f12'
 t = 'Key.f10'
 f = 'Key.f9'
 #Dateipfad
-dpfad = "G:/"
+dpfad = "G:\Raw\Marker"
 #Dateiname
 date = "%Y-%m-%d %H-%M-%S"
 #Erstelt bie Nox7-Media @ Julian Schiefer und Maximilian Blinzler
